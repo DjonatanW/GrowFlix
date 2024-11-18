efdf51b9922d0224514a4c69684255d4421b6753
Djonatan Weber <djonatan11021996@gmail.com>
2024-11-18 20:51:36 -0300

    feat: create the new project.

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+const movies = [
+  {
+    img: 'https://img.youtuve.com/vi/aJ-HZuLcKeA/sddefault.jpg',
+    title: 'Growcast #00 | Conheça a Growdev',
+    link: 'https://www.youtube.com/embed/aJ-HZuLcKeA',
+    category: 'growcast'
+  }
+]
+
+const general = movies.filter()
